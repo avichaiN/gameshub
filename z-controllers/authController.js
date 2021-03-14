0d79d1411979395c2251ab3132f0e268063b3b27
Avichai Nissim <avichai74@gmail.com>
2021-03-14 14:21:01 +0200

    1

--- z-controllers/authController.js
+++ z-controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require("../z-models/user");
+const User = require("../z-models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jwt-simple");
 const nodemailer = require("nodemailer");
